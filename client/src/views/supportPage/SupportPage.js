7721aa9fec150be98f39f5e325f94db64ab602ae
Cenxus <urs_ole@hotmail.com>
2021-02-09 17:03:32 +0100

    Update SupportPage.js
    Implemented Form instead of input, improved code

--- client/src/views/supportPage/SupportPage.js
+++ client/src/views/supportPage/SupportPage.js
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import styles from './SupportPage.module.css';
 
 const SupportPage = () => {
@@ -35,104 +35,80 @@ const SupportPage = () => {
 				</Col>
 			</Row>
 			<Row>
-				<Col xl={6} className="mx-auto">
+				<Col className={'text-center'}>
 					<p>Send oss en mail i feltet under</p>
 				</Col>
 			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<p>Navn:</p>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<input></input>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<p>E-post:</p>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<input></input>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<p>Telefon</p>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<input></input>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<p>Type problem:</p>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<input></input>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<p>Melding:</p>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
-					<textarea rows="5"></textarea>
-				</Col>
-			</Row>
-			<Row>
-				<Col xl={6} className="mx-auto">
+			<div className="mx-auto" style={{width: "50%"}}>
+				<Form>
+					<Form.Group controlId="supportForm.ControlInputName">
+						<Form.Label>Navn:</Form.Label>
+						<Form.Control/>
+					</Form.Group>
+					<Form.Group controlId="supportForm.ControlInputEmail">
+						<Form.Label>E-post:</Form.Label>
+						<Form.Control type="email" placeholder="navn@eksempel.no" />
+					</Form.Group>
+					<Form.Group controlId="supportForm.ControlInputPhone">
+						<Form.Label>Telefon:</Form.Label>
+						<Form.Control/>
+					</Form.Group>
+					<Form.Group controlId="supportForm.ControlInputProblem">
+						<Form.Label>Type problem:</Form.Label>
+						<Form.Control/>
+					</Form.Group>
+					<Form.Group controlId="exampleForm.ControlTextareaMessage">
+						<Form.Label>Melding:</Form.Label>
+						<Form.Control as="textarea" rows={5} />
+					</Form.Group>
 					<Button className="btn-warning">Send inn</Button>
-				</Col>
-			</Row>
+				</Form>
+			</div>
+			
 			<Row>
 				<Col className="text-center pt-5">
 					<p>Eller kontakt oss direkte</p>
 				</Col>
 			</Row>
-			<Row>
-				<Col className="text-center pt-5">
-					<p>Hans Petter Sandvold</p>
-				</Col>
-				<Col className="text-center pt-5">
-					<p>Kenneth Nygård</p>
-				</Col>
-			</Row>
-			<Row xl="6" className="text-center">
-				<Col className="offset-md-1">
-					<p>Telefon</p>
-				</Col>
-				<Col>
-					<b>456 20 830</b>
-				</Col>
-				<Col className="offset-md-2">
-					<p>Telefon</p>
-				</Col>
-				<Col>
-					<b>654 45 560</b>
-				</Col>
-			</Row>
-			<Row xl="6" className="text-center">
-				<Col className="offset-md-1">
-					<p>E-post</p>
+			
+			<Row className="mx-auto">
+				<Col md={{offset: 2}}>
+					<div>
+						<Form>
+							<Form.Label>Hans Petter Sandvold</Form.Label>
+							<Form.Group as={Row}>
+								<Form.Label column sm={4}>Telefon:</Form.Label>
+								<Col>
+									<Form.Control plaintext readOnly value="456 20 830"/>
+								</Col>
+							</Form.Group>
+							<Form.Group as={Row}>
+								<Form.Label column sm={4}>E-post:</Form.Label>
+								<Col>
+									<Form.Control plaintext readOnly value="hanspetter@wellness.no"/>
+								</Col>
+							</Form.Group>
+						</Form>
+					</div>
 				</Col>
 				<Col>
-					<b>hanspetter@wellness.no</b>
-				</Col>
-				<Col className="offset-md-2">
-					<p>E-post</p>
-				</Col>
-				<Col>
-					<b>kenneth@wellness.no</b>
+					<div>
+						<Form>
+							<Form.Label>Kenneth Nygård</Form.Label>
+							<Form.Group as={Row}>
+								<Form.Label column sm={4}>Telefon</Form.Label>
+								<Col>
+									<Form.Control plaintext readOnly value="654 45 560"/>
+								</Col>
+							</Form.Group>
+							<Form.Group as={Row}>
+								<Form.Label column sm={4}>E-post</Form.Label>
+								<Col>
+									<Form.Control plaintext readOnly value="kenneth@wellness.no"/>
+								</Col>
+							</Form.Group>
+						</Form>
+					</div>
 				</Col>
 			</Row>
 		</Container>
