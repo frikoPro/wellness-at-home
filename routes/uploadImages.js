e6c668199c88518f00b653906c49dc52bc30fcf7
FrikoPro <jansenfredrik@gmail.com>
2021-02-11 00:03:01 +0100

    You can now upload multiple files

--- routes/uploadImages.js
+++ routes/uploadImages.js
@@ -14,26 +14,19 @@ router.route('/upload').post((req, res) => {
 	// 'profile_pic' is the name of our file input field in the HTML form
 	let upload = multer({
 		storage: storage,
-	}).single('file');
+	}).array('multi-files');
 
 	upload(req, res, function (err) {
 		// req.file contains information of uploaded file
 		// req.body contains information of text fields, if there were any
 
-		if (req.fileValidationError) {
-			return res.send(req.fileValidationError);
-		} else if (!req.file) {
-			return res.send('Please select an image to upload');
-		} else if (err instanceof multer.MulterError) {
-			return res.send(err);
-		} else if (err) {
-			return res.send(err);
+		if (err) {
+			return res.send('Error: ', err);
 		}
 
-		// Display uploaded image for user validation
-		res.send(
-			`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`
-		);
+		// req.files.forEach((file) => res.send(file.path));
+
+		res.send(req.files);
 	});
 });
 
