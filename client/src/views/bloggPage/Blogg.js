ad2409e619de35407bc4fb9bd8b2f213e92e7c52
Ole Norholm <olenorholm55@gmail.com>
2021-02-11 19:38:07 +0100

    It finally works, just need to add some styling

--- client/src/views/bloggPage/Blogg.js
+++ client/src/views/bloggPage/Blogg.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from 'react';
+import {Row} from "react-bootstrap";
 
 const axios = require('axios');
 
@@ -9,22 +10,41 @@ const Blogg = () => {
     let [fbData, setFbData] = useState([]);
 
     useEffect(() => {
-        axios.get(url).then((response) => setFbData(response.data.data));
+        axios.get(url).then((response) => {
+            const data = response.data.data;
+            const newData = data.map(item => {
+                return {...item, page_id: item.id.split("_")[0], post_id: item.id.split("_")[1]}
+            })
+            console.log(newData)
+            setFbData(newData)
+            window?.FB?.XFBML?.parse() //Loads the JS SDK again so that the "slower" elements gets detected when they render
+        });
     }, [url]);
+
 //       style={{margin: '5px', padding: '5px', width: 800, height: 680}}>
-    return (
-        <>
-            {fbData.map((post) => (
-                    <div
-                        className="fb-post"
-                        data-href={`https://www.facebook.com/102417811874407/posts/${post.id.split("_")[1]}`}
-                        data-width="auto"
-                        data-show-text="true"
-                        // style={{height: 1000}}
-                    />
-                )
-            )}
-        </>
+    // <div className="fb-post" data-href="https://www.facebook.com/102417811874407/posts/106722811443907/" data-width="500" data-show-text="true"></div>
+
+
+    return (<div> {fbData.map((post) => (
+                <div style={{paddingTop: 40}}
+                     className="col-lg-5 col-md-5 col-sm-5 container justify-content-center">
+                    <Row>
+                        <div
+                            key={post.post_id}
+                            className="fb-post"
+                            data-href={`https://www.facebook.com/102417811874407/posts/${post.post_id}/`}
+                            data-width="500"
+                            data-show-text="true"
+                        />
+                    </Row>
+
+
+                </div>
+
+            )
+        )} </div>
+
     );
 };
+
 export default Blogg;
