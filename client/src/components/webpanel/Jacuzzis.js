84b4603de3caab5dd99870d43bfbfd95433ec086
FrikoPro <jansenfredrik@gmail.com>
2021-02-12 19:46:51 +0100

    webpanel

--- client/src/components/webpanel/Jacuzzis.js
+++ client/src/components/webpanel/Jacuzzis.js
@@ -8,9 +8,19 @@ const Jacuzzis = () => {
 		brand: '',
 		description: '',
 		price: '',
-		techSpec: [],
-		relatedProducts: [],
+		techSpec: [
+			{ property: 'strømforbruk', value: '200kw/h' },
+			{ property: 'strømforbruk', value: '200kw/h' },
+			{ property: 'strømforbruk', value: '200kw/h' },
+		],
+		relatedProducts: [{}],
 		images: [],
+	});
+
+	const [newTechSpec, setNewTechSpec] = useState({
+		properties: ['vannsystem', 'strømforbruk', 'kapasitet'],
+		newProperty: '',
+		selectedTechSpec: {},
 	});
 
 	const [images, setImages] = useState([]);
@@ -43,6 +53,17 @@ const Jacuzzis = () => {
 
 		setNewProduct({ ...filenames });
 		setImages([...e.target.files]);
+	};
+
+	const addNewProperty = () => {
+		if (newTechSpec.property && newTechSpec.value) {
+			return setNewProduct({
+				...newProduct,
+				techSpec: [...newProduct.techSpec, newTechSpec.selectedTechSpec],
+			});
+		}
+
+		console.log('ikke fylt inn');
 	};
 
 	return (
@@ -98,12 +119,87 @@ const Jacuzzis = () => {
 					</Form.Group>
 					<Form.Group>
 						<Form.Label>Tekniske spesifikasjoner</Form.Label>
-						<Form.Control
-							placeholder="tekniske spesifikasjoner"
-							onChange={(e) =>
-								setNewProduct({ ...newProduct, techSpec: e.target.value })
-							}
-						/>
+						<Row>
+							<Col sm={2}>
+								<select
+									className="form-control"
+									placeholder="Strømforbruk"
+									onChange={(e) =>
+										setNewTechSpec({
+											...newTechSpec,
+											selectedTechSpec: {
+												...newTechSpec,
+												property: e.target.value,
+											},
+										})
+									}>
+									<option disabled selected>
+										Velg egenskap
+									</option>
+									{newTechSpec.properties.map((option) => (
+										<option>{option}</option>
+									))}
+								</select>
+							</Col>
+							<Col sm={1} className="text-center">
+								<div>:</div>
+							</Col>
+							<Col sm={2}>
+								<Form.Control
+									placeholder="200kw/h"
+									onChange={(e) =>
+										setNewTechSpec({
+											...newTechSpec,
+											selectedTechSpec: {
+												...newTechSpec.selectedTechSpec,
+												value: e.target.value,
+											},
+										})
+									}
+								/>
+							</Col>
+							<Col>
+								<Button onClick={() => addNewProperty()}>Legg til</Button>
+							</Col>
+							<Col>
+								<ul style={{ listStyleType: 'none' }}>
+									{newProduct.techSpec.map((item) => (
+										<li>
+											{item.property ? item.property + ' : ' + item.value : ''}
+										</li>
+									))}
+								</ul>
+							</Col>
+						</Row>
+
+						<Row className="mt-5">
+							<Col sm={2}>
+								<Form.Control
+									placeholder="legg til ny egenskap"
+									onChange={(e) =>
+										setNewTechSpec({
+											...newTechSpec,
+											newProperty: e.target.value,
+										})
+									}
+								/>
+							</Col>
+							<Col>
+								<Button
+									className="btn-success"
+									onClick={() =>
+										setNewTechSpec({
+											...newTechSpec,
+											properties: [
+												...newTechSpec.properties,
+												newTechSpec.newProperty,
+											],
+										})
+									}>
+									Legg til
+								</Button>
+							</Col>
+						</Row>
 					</Form.Group>
 					<Form.Group>
 						<Form.Label>Relaterte produkter</Form.Label>
