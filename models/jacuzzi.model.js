a7b74a9d754bb015697cac10c85a5b2298b31402
FrikoPro <jansenfredrik@gmail.com>
2021-02-13 16:45:45 +0100

    commit

--- models/jacuzzi.model.js
+++ models/jacuzzi.model.js
@@ -3,24 +3,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserReviews = new Schema(
-	{
-		rating: Number,
-		author: String,
-		header: String,
-		text: String,
-	},
-	{ timestamps: true }
+  {
+    rating: Number,
+    author: String,
+    header: String,
+    text: String,
+  },
+  { timestamps: true }
 );
 
 const JacuzzisSchema = new Schema({
-	name: { type: String, required: true, unique: true },
-	brand: String,
-	images: [String],
-	aboutProduct: String,
-	price: String,
-	techSpec: { type: Array, unique: true },
-	relatedProducts: [String],
-	userReviews: [UserReviews],
+  name: { type: String, required: true, unique: true },
+  brand: String,
+  images: [String],
+  aboutProduct: String,
+  price: String,
+  techSpec: { type: Array, unique: true },
+  relatedProducts: [Object],
+  userReviews: [UserReviews],
 });
 
 const Jacuzzis = mongoose.model('Jacuzzi', JacuzzisSchema);
