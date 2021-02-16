1794a20c770ae2fcb1402e8f11e025ab38b2b65e
Ole Norholm <olenorholm55@gmail.com>
2021-02-16 15:44:46 +0100

    updated fake array

--- client/src/views/eventsPage/Events.js
+++ client/src/views/eventsPage/Events.js
@@ -6,47 +6,71 @@ import {renderToStaticMarkup} from "react-dom/server";
 const Events = () => {
 let time;
 //todo: legg til bilder i arrayet
+    // OBS calendar must be set as variable event
 const eventsData = [
         {
+            create_date: 1618054898,
             date: 1618054898,
             City: "Drammen",
             Address: "DrammenVegen 22",
             Location: "Drammenshallen",
             Pos: 0,
-            Calendar: "",
-            Img: "https://via.placeholder.com/200x100"
+            Img: "https://via.placeholder.com/200x100",
+            Calendar: {
+                title: "My birthday party",
+                description: "Be there!",
+                start: "2019-12-29 18:00:00 +0100",
+                allDay: true
+            }
 
         },
         {
+            create_date: 1618054898,
             date: 1620646898,
             City: "Oslo/Fornebu",
             Address: "OsloVegen 21",
             Location: "Telenor Arena",
             Pos: 0,
-            Calendar: "",
-            Img: "https://via.placeholder.com/200x100"
+            Img: "https://via.placeholder.com/200x100",
+            Calendar: {
+                title: "My birthday party",
+                description: "Be there!",
+                start: "2019-12-29 18:00:00 +0100",
+                allDay: true
+            }
 
         },
         {
+            create_date: 1618054898,
             date: 	1652182898,
             City: "Kristiansand",
             Address: "OsloVegen 21",
             Location: "Telenor Arena",
             Pos: 0,
-            Calendar: "",
-            Img: "https://via.placeholder.com/200x100"
+            Img: "https://via.placeholder.com/200x100",
+            Calendar: {
+                title: "My birthday party",
+                description: "Be there!",
+                start: "2019-12-29 18:00:00 +0100",
+                allDay: true
+            }
 
         },
         {
+            create_date: 1618054898,
             date: 1778413298,
             City: "Trondheim",
             Address: "TrondheimVegen 108",
             Location: "Trondheim fotball Arena",
             Pos: 0,
-            Calendar: "",
-            Img: "https://via.placeholder.com/200x100"
-
-        },
+            Img: "https://via.placeholder.com/200x100",
+            Calendar: {
+                title: "My birthday party",
+                description: "Be there!",
+                start: "2019-12-29 18:00:00 +0100",
+                allDay: true
+            }
+        }
     ]
 // border="primary" style={{marginTop: 40}}
     return (
