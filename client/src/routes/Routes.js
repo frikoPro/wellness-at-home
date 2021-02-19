75e76bdf4028793e487acd7df24c1a53966d03c1
Ole Norholm <olenorholm55@gmail.com>
2021-02-19 11:44:45 +0100

    Added routing to elements in events.js

--- client/src/routes/Routes.js
+++ client/src/routes/Routes.js
@@ -1,56 +1,58 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Footer from '../components/Footer';
 import NavigationBar from '../components/navbar/NavigationBar';
 import Test from '../components/Test';
-import { ScrollProvider } from '../contexts/ScrollContext';
+import {ScrollProvider} from '../contexts/ScrollContext';
 import HomePage from '../views/homePage/HomePage';
 import JacuzziPage from '../views/jacuzziPage/JacuzziPage';
 import Blogg from '../views/bloggPage/Blogg';
 import SupportPage from '../views/supportPage/SupportPage';
 import Webpanel from '../views/webpanel/Webpanel';
 import Events from '../views/eventsPage/Events';
-import { JacuzziProvider } from '../contexts/JacuzziContext';
-import { ProductsProvider } from '../contexts/ProductsContext';
+import {JacuzziProvider} from '../contexts/JacuzziContext';
+import {ProductsProvider} from '../contexts/ProductsContext';
+import EventPage from "../views/eventsPage/EventPage";
+import history from "../history.js";
 
 const Routes = () => {
-	return (
-		<BrowserRouter>
-			<ScrollProvider>
-				<header>
-					<NavigationBar />
-				</header>
-				<main>
-					<JacuzziProvider>
-						<ProductsProvider>
-							<Switch>
-								<Route exact path="/">
-									<HomePage />
-								</Route>
-								<Route path="/test">
-									<Test />
-								</Route>
-								<Route path="/blogg">
-									<Blogg />
-								</Route>
-								<Route path="/Arrangementer">
-									<Events />
-								</Route>
-								<Route path="/Kundeserivce">
-									<SupportPage />
-								</Route>
-								<Route path="/spabad/:id">
-									<JacuzziPage />
-								</Route>
-								<Route path="/webpanel" component={Webpanel} />
-							</Switch>
-						</ProductsProvider>
-					</JacuzziProvider>
-				</main>
-			</ScrollProvider>
+    return (
+        <BrowserRouter history={history}>
+            <ScrollProvider>
+                <header>
+                    <NavigationBar/>
+                </header>
+                <main>
+                    <JacuzziProvider>
+                        <ProductsProvider>
+                            <Switch>
+                                <Route exact path="/">
+                                    <HomePage/>
+                                </Route>
+                                <Route path="/test">
+                                    <Test/>
+                                </Route>
+                                <Route path="/blogg">
+                                    <Blogg/>
+                                </Route>
+                                <Route path="/Arrangementer">
+                                    <Events/>
+                                </Route>
+                                <Route path="/Kundeserivce">
+                                    <SupportPage/>
+                                </Route>
+                                <Route path="/spabad/:id">
+                                    <JacuzziPage/>
+                                </Route>
+                                <Route path="/webpanel" component={Webpanel}/>
+                            </Switch>
+                        </ProductsProvider>
+                    </JacuzziProvider>
+                </main>
+            </ScrollProvider>
 
-			<Footer />
-		</BrowserRouter>
-	);
+            <Footer/>
+        </BrowserRouter>
+    );
 };
 
 export default Routes;
