88782d5f6ba11fcdce87679a9318714e6115935b
FrikoPro <jansenfredrik@gmail.com>
2021-02-22 12:09:47 +0100

    Fixed sendmail

--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,49 +1,43 @@
-var express = require('express');
-var router = express.Router();
-var nodemailer = require('nodemailer');
-var cors = require('cors');
+const router = require('express').Router();
+const nodemailer = require('nodemailer');
 
 let transport = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    secure: false,
-    auth: {
-        user: 'jackson16@ethereal.email',
-        pass: 'WhQcZbfSMpYvWNqe5w',
-    },
+	host: 'smtp.ethereal.email',
+	port: 587,
+	secure: false,
+	auth: {
+		user: 'jackson16@ethereal.email',
+		pass: 'WhQcZbfSMpYvWNqe5w',
+	},
 });
 
-transport.verify(function(error, success) {
-    if(error) {
-        console.log(error);
-    } else {
-        console.log("Server is ready to take our messages");
-    }
+transport.verify(function (error, success) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Server is ready to take our messages');
+	}
 });
 
 router.post('/send', (req, res, next) => {
-    var name = req.body.name,
-    var email = req.body.email,
-    var phone = req.body.phone,
-    var problem = req.body.problem,
-    var message = req.body.message,
+	var mail = {
+		from: req.body.name,
+		to: 'urs_ole@hotmail.com',
+		subject: req.body.problem,
+		text: req.body.message,
+	};
 
-    var mail = {
-        from: name, email, phone,
-        to: "urs_ole@hotmail.com",
-        subject: problem,
-        text: message
-    }
+	transport.sendMail(mail, (err, data) => {
+		if (err) {
+			res.json({
+				status: 'fail',
+			});
+		} else {
+			res.json({
+				status: 'success',
+			});
+		}
+	});
+});
 
-    transport.sendMail(mail, (err, data) => {
-        if(err) {
-            res.json({
-                status: 'fail'
-            })
-        } else {
-            res.json({
-                status: 'success'
-            })
-        }
-    })
-})
+module.exports = router;
