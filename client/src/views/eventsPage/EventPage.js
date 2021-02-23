95c6ec60af28d89de32070b93f79ba5c4231025f
Ole Norholm <olenorholm55@gmail.com>
2021-02-23 16:32:03 +0100

    Googlemaps and calendar links have been added

--- client/src/views/eventsPage/EventPage.js
+++ client/src/views/eventsPage/EventPage.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import Gmaps from "../../components/Gmaps";
+import CalendarLink from "../../components/ CalendarLink";
 
 const EventPage = (props) => {
-    console.log(props)
     return (
         <>
             <h1>Hello world! - {props.location}</h1>
             <Gmaps {...props.pos}/>
+            <CalendarLink {...props.calendar}/>
         </>
     );
 
 };
 
 export default EventPage;
-
