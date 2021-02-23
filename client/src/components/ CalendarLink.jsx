fd59f27ba134694c4810614483f2543c870579a9
Ole Norholm <olenorholm55@gmail.com>
2021-02-23 16:39:03 +0100

    Calendar links now open in new tabs

--- client/src/components/ CalendarLink.jsx
+++ client/src/components/ CalendarLink.jsx
@@ -1,27 +1,20 @@
 import {Dropdown} from "react-bootstrap";
 import { google, outlook, office365, yahoo, ics } from "calendar-link";
 
-
 const CalendarLink = (props) => {
-
-console.log(props)
-    const openInNewTab = (url) => {
-        const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
-        if (newWindow) newWindow.opener = null
-    }
     return (
         <>
-            
+
             <Dropdown>
                 <Dropdown.Toggle variant="success" id="dropdown-basic">
                     Legg til i kalender
                 </Dropdown.Toggle>
                 <Dropdown.Menu>
-                    <Dropdown.Item href={ics(props)}>ICS(Apple)</Dropdown.Item>
-                    <Dropdown.Item href={google(props)}>Google</Dropdown.Item>
-                    <Dropdown.Item href={outlook(props)}>Outlook</Dropdown.Item>
-                    <Dropdown.Item href={office365(props)}>Office365</Dropdown.Item>
-                    <Dropdown.Item href={yahoo(props)}>Yahoo</Dropdown.Item>
+                    <Dropdown.Item href={ics(props)} target="_blank" rel="noreferrer noopener">ICS(Apple)</Dropdown.Item>
+                    <Dropdown.Item href={google(props)} target="_blank" rel="noreferrer noopener">Google</Dropdown.Item>
+                    <Dropdown.Item href={outlook(props)} target="_blank" rel="noreferrer noopener">Outlook</Dropdown.Item>
+                    <Dropdown.Item href={office365(props)} target="_blank" rel="noreferrer noopener">Office365</Dropdown.Item>
+                    <Dropdown.Item href={yahoo(props)} target="_blank" rel="noreferrer noopener">Yahoo</Dropdown.Item>
                 </Dropdown.Menu>
             </Dropdown>
         </>
