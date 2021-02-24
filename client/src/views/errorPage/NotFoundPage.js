3153dff4e50bc42e9b0f9ba6bb5e17c39fbd1cc2
Ole Norholm <olenorholm55@gmail.com>
2021-02-24 15:10:28 +0100

    Notfoundpage has content now

--- client/src/views/errorPage/NotFoundPage.js
+++ client/src/views/errorPage/NotFoundPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import {useParams} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 const NotFoundPage = () => {
 
     return (
         <>
-            <h1>404: Page not found!</h1>
+            <div  style={{textAlign: "center"}}>
+                <h1>404</h1>
+                <h2>Siden du prøver å få tilgang til finnes ikke!</h2>
+                <h2>Venligst prøv igjen, eller kontakt oss for hjelp.</h2>
+                <div>
+                    <p>
+                        <Link to="/Arrangementer">Gå tilbake</Link>
+                    </p>
+                    <p>
+                        <Link to="/">Hjem</Link>
+                    </p>
+                </div>
+            </div>
         </>
     );
 };
--- client/src/views/eventsPage/EventPage.js
+++ client/src/views/eventsPage/EventPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Gmaps from "../../components/Gmaps";
 import CalendarLink from "../../components/ CalendarLink";
+import {Link} from "react-router-dom";
 
 const EventPage = (props) => {
     return (
         <>
+            <div>
+                <p>
+                    <Link to="/Arrangementer">Tilbake</Link>
+                </p>
+            </div>
             <h1>Hello world! - {props.location}</h1>
             <Gmaps {...props.pos}/>
             <CalendarLink {...props.calendar}/>
