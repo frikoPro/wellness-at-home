d612f1b1c0af6c76fed03dfd1b6a722fe69d54b1
Ole Norholm <olenorholm55@gmail.com>
2021-02-26 11:13:45 +0100

    Revert "Merge branch 'master' of https://github.com/FrikoPro/wellness-at-home"
    This reverts commit 3f1979effe2fd61d374815ef18e4102c5944aa6f, reversing
    changes made to fcff0555d54a8629c881bc401aedbf0c0cbabfac.

--- client/src/components/navbar/DropdownSection.js
+++ client/src/components/navbar/DropdownSection.js
@@ -1,23 +1,23 @@
 import { Col } from 'react-bootstrap';
 
 const DropdownSection = ({ section, styling }) => {
-  return section.map((item, i) => (
-    <Col className="text-center" key={i}>
-      <a href="/svenskabadpro">
-        <h5
-          className={`m-3 nav-text-color hover-gold ${styling.dropmenuItemHeader}`}>
-          {item.brand}
-        </h5>
-      </a>
-      {item.jacuzzis.map((jacuzzi, i) => (
-        <a href={`/spabad/${jacuzzi}`} key={i}>
-          <p className={`${styling.dropmenuItem} nav-text-color hover-gold`}>
-            {jacuzzi}
-          </p>
-        </a>
-      ))}
-    </Col>
-  ));
+	return section.map((item) => (
+		<Col className="text-center">
+			<a href="/svenskabadpro">
+				<h5
+					className={`m-3 nav-text-color hover-gold ${styling.dropmenuItemHeader}`}>
+					{item.brand}
+				</h5>
+			</a>
+			{item.jacuzzis.map((jacuzzi) => (
+				<a href={`/spabad/${jacuzzi}`}>
+					<p className={`${styling.dropmenuItem} nav-text-color hover-gold`}>
+						{jacuzzi}
+					</p>
+				</a>
+			))}
+		</Col>
+	));
 };
 
 export default DropdownSection;
--- routes/products.js
+++ routes/products.js
@@ -2,64 +2,56 @@ const router = require('express').Router();
 let Product = require('../models/product.model');
 
 router.route('/').get((req, res) => {
-  Product.find()
-    .then((products) => res.json(products))
-    .catch((err) => res.status(400).json('Error: ', err));
+	Product.find()
+		.then((products) => res.json(products))
+		.catch((err) => res.status(400).json('Error: ', err));
 });
 
 router.route('/add').post(async (req, res, next) => {
-  const name = req.body.name;
-  const affiliation = req.body.affiliation;
-  const images = req.body.images;
-  const category = req.body.category;
-  const aboutProduct = req.body.aboutProduct;
-  const price = req.body.price;
-  const techSpec = req.body.techSpec;
-  const relatedProducts = req.body.relatedProducts;
+	const name = req.body.name;
+	const affiliation = req.body.affiliation;
+	const images = req.body.images;
+	const category = req.body.category;
+	const aboutProduct = req.body.aboutProduct;
+	const price = req.body.price;
+	const techSpec = req.body.techSpec;
+	const relatedProducts = req.body.relatedProducts;
 
-  const newProduct = new Product({
-    name,
-    affiliation,
-    category,
-    images,
-    aboutProduct,
-    price,
-    techSpec,
-    relatedProducts,
-  });
+	const newProduct = new Product({
+		name,
+		affiliation,
+		category,
+		images,
+		aboutProduct,
+		price,
+		techSpec,
+		relatedProducts,
+	});
 
-  try {
-    await newProduct.save();
-    res.status(200).json('produkt lagt inn');
-  } catch (err) {
-    next(err);
-  }
+	try {
+		await newProduct.save();
+		res.status(200).json('produkt lagt inn');
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.route('/:id').delete(async (req, res, next) => {
-  try {
-    await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json('Produktet er slettet');
-  } catch (err) {
-    next(err);
-  }
+router.route('/:id').delete(async (req, res) => {
+	try {
+		await Product.findByIdAndDelete(req.params.id);
+		res.status(200).json('Produktet er slettet');
+	} catch (err) {
+		res.status(400).json(err);
+	}
 });
 
-router.route('/:id').patch(async (req, res, next) => {
-  try {
-    const body = { ...req.body };
-
-    delete body._id;
-
-    let updatedProduct = new Product({ ...body });
-
-    await updatedProduct.save();
-
-    await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json('Produktet er oppdatert');
-  } catch (err) {
-    next(err);
-  }
+router.route('/:id').patch(async (req, res) => {
+	try {
+		await Product.findByIdAndUpdate({ _id: req.params.id }, { ...req.body });
+		res.status(200).json('Produktet er oppdatert');
+	} catch (err) {
+		next(err);
+	}
 });
 
 module.exports = router;
