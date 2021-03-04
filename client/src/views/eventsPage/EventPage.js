093d0722302a2045a82b163d1a49c743b848c77d
Ole Norholm <olenorholm55@gmail.com>
2021-03-04 16:07:42 +0100

    Removed unsued var and styled a few pages

--- client/src/views/eventsPage/EventPage.js
+++ client/src/views/eventsPage/EventPage.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react';
 import Gmaps from "../../components/Gmaps";
 import CalendarLink from "../../components/ CalendarLink";
-import {Link} from "react-router-dom";
 import {Breadcrumb, Tab, Tabs} from "react-bootstrap";
 import styles from "./EventPage.module.css";
 import Calendar from "react-calendar";
@@ -16,27 +15,31 @@ const EventPage = (props) => {
                 <Breadcrumb>
                     <Breadcrumb.Item href="/">Hjem</Breadcrumb.Item>
                     <Breadcrumb.Item href="/Arrangementer">Arrangementer</Breadcrumb.Item>
+                    <Breadcrumb.Item href="">{props.location}</Breadcrumb.Item>
                 </Breadcrumb>
             </div>
-            <div>
-                <h1>Wellness at home - {props.location}</h1>
-                <img src={"https://cdn.discordapp.com/attachments/811250538443833374/811252239552479232/AURUA_head_sbiwebb-1024x587.jpg"}
-                     class="d-flex justify-content-center"/>
+                <img src={"https://via.placeholder.com/1200x500"}
+                     class="d-flex justify-content-center" className={`${styles.bannerImg}`}/>
+            <div className={`${styles.mainContainer}`}>
+                     <div className={`${styles.title}`}>
+                         <h1>Wellness at home - {props.location}</h1>
+                     </div>
                 <div className={`${styles.tabsContainer}`}>
-                    <Tabs defaultActiveKey="main" id="tab" >
+                    <Tabs defaultActiveKey="main" id="tab" className={`${styles.tab}`}>
                         <Tab eventKey="main" title="Main info" className={`${styles.tabs}`} >
-                            <h1>Hello world! - {props.location}</h1>
+                            <h1>Tider</h1>
+                            <h4>kl 10-11</h4>
+                            <div className={`${styles.mainSideView}`}>
+                                <CalendarLink {...props.calendar}/>
+                                <Calendar
+                                    onChange={onChange}
+                                    showWeekNumbers
+                                    value={value}
+                                />
+                            </div>
                         </Tab>
                         <Tab eventKey="map" title="Kart og transport" className={`${styles.tabs}`} >
                             <Gmaps {...props.pos}/>
-                        </Tab>
-                        <Tab eventKey="calendar" title="Kalender" className={`${styles.tabs}`} >
-                            <Calendar
-                                onChange={onChange}
-                                showWeekNumbers
-                                value={value}
-                            />
-                            <CalendarLink {...props.calendar}/>
                         </Tab>
                     </Tabs>
                 </div>
