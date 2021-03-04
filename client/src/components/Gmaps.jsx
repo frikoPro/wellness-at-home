8a139443301b1726a6f796d9315f2a5cc492677e
Ole Norholm <olenorholm55@gmail.com>
2021-03-04 19:15:25 +0100

    Got infoWindow up, need styling

--- client/src/components/Gmaps.jsx
+++ client/src/components/Gmaps.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
-import {GoogleMap, useJsApiLoader, Marker} from '@react-google-maps/api';
+import React, {useState} from 'react'
+import {GoogleMap, useJsApiLoader, Marker, InfoWindow, MarkerProps } from '@react-google-maps/api';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import {google} from "calendar-link";
 
 const api = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
@@ -9,6 +12,8 @@ function Gmaps(props) {
         googleMapsApiKey: api
     })
 
+    const [chosenMarker, setChosenMarker] = useState({})
+
     const initialPos = {
         lat: props.lat,
         lng: props.lng
@@ -17,6 +22,14 @@ function Gmaps(props) {
         width: 'auto',
         height: '500px'
     };
+    const onLoad = infoWindow => {
+        console.log('infoWindow: ', infoWindow)
+    }
+    const divStyle = {
+        background: `white`,
+        border: `1px solid #ccc`,
+        padding: 15
+    }
 
     return isLoaded ? (
         <GoogleMap
@@ -26,10 +39,24 @@ function Gmaps(props) {
         >
             <>
                 <Marker position={initialPos}
-                        name={'Your position'}
-
+                        icon={"https://icons.iconarchive.com/icons/paomedia/small-n-flat/32/map-marker-icon.png"}
+                        onClick={() => setChosenMarker(
+                            {title:"Title", subTitle: "HelloWorld!!", link:"https://fontawesome.com/how-to-use/on-the-web/using-with/react", coordinates: [props.lat, props.lng] }
+                            )}
                 />
-
+                {Object.keys(chosenMarker).length !== 0 ?
+                <InfoWindow
+                    onLoad={onLoad}
+                    position={{lat: chosenMarker.coordinates[0],lng: chosenMarker.coordinates[1] }}
+                    onCloseClick={() => setChosenMarker({})}
+                >
+                    <div style={divStyle}>
+                        <h1>{chosenMarker.title}</h1>
+                        <h1>{chosenMarker.subTitle}</h1>
+                        <h1>{chosenMarker.link}</h1>
+                        <h1>{chosenMarker.coordinates}</h1>
+                    </div>
+                </InfoWindow> : undefined}
             </>
         </GoogleMap>
     ) : <></>
