885a4a8776f8cf77900b18c6c7855372d0b52d15
Ole Norholm <olenorholm55@gmail.com>
2021-03-08 13:05:56 +0100

    Minor styling changes

--- client/src/views/bloggPage/Blogg.js
+++ client/src/views/bloggPage/Blogg.js
@@ -24,22 +24,21 @@ const Blogg = () => {
 //       style={{margin: '5px', padding: '5px', width: 800, height: 680}}>
     // <div className="fb-post" data-href="https://www.facebook.com/102417811874407/posts/106722811443907/" data-width="500" data-show-text="true"></div>
 
-    const bsNumElement = 4; // For development purposes
-    return (<>
+    const bsNumElement = 3; // For development purposes
+    return (
+        <>
             {fbData.map((post) => (
-                <div style={{paddingTop: 40}}>
-                    <Card style={{padding: 40, boxShadow: "5px 5px 5px #b3b3b3"}}
-                          className={`col-lg-${bsNumElement} col-md-${bsNumElement} col-sm-${bsNumElement} container justify-content-center`}>
-                        <Card.Body>
-                            <div
-                                key={post.post_id} //PostID is unique and a good key
-                                className="fb-post"
-                                data-href={`https://www.facebook.com/102417811874407/posts/${post.post_id}/`}
-                                data-width="500"
-                                data-show-text="true"
-                            />
-                        </Card.Body>
-                    </Card>
+                <div style={{paddingTop: 20}}>
+                    <div className={`col-lg-${bsNumElement} col-md-${bsNumElement} col-sm-${bsNumElement} container justify-content-center`}>
+                        <div
+                            style={{boxShadow: "5px 5px 5px #b3b3b3"}}
+                            key={post.post_id} //PostID is unique and a good key
+                            className="fb-post"
+                            data-href={`https://www.facebook.com/102417811874407/posts/${post.post_id}/`}
+                            data-width="500"
+                            data-show-text="true"
+                        />
+                    </div >
                 </div>
                 )
             )}
