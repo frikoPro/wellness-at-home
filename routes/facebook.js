fc06122e7add709a69dfada9d260b83636aa8f94
FrikoPro <jansenfredrik@gmail.com>
2021-03-10 11:54:04 +0100

    fixed facebook api one more time

---
+++ routes/facebook.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+const router = require('express').Router();
+
+router.route('/').get((req, res) => {
+  const url = `https://graph.facebook.com/${process.env.FACEBOOK_ID}/feed?access_token=${process.env.FACEBOOK_TOKEN}`;
+
+  axios
+    .get(url)
+    .then((response) => res.status(200).json(response.data))
+    .catch((err) => res.status(400).send(err));
+});
+
+module.exports = router;
