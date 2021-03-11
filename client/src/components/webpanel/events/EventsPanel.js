e73b80fe54254f23fea4e9e07162d94c89fb6532
Ole Norholm <olenorholm55@gmail.com>
2021-03-11 11:33:38 +0100

    Destructed time and date and passed to parent comp

--- client/src/components/webpanel/events/EventsPanel.js
+++ client/src/components/webpanel/events/EventsPanel.js
@@ -1,11 +1,34 @@
 import {Button, Card, Col, Form, Row} from "react-bootstrap";
-import React from "react";
+import React, {useState} from "react";
 import EventsPanelDate from "./EventsPanelDate";
 import EventsPanelMap from "./EventsPanelMap";
 import EventsPanelImg from "./EventsPanelImg";
 import EventsPanelCalendar from "./EventsPanelCalendar";
 
 const EventsPanel = () => {
+	const [event, setEvent] = useState({
+		date: {
+			start: 1615200485,
+			end: 1615546085,
+		},
+		city: "Drammen",
+		address: "Drammenvegen 22",
+		location: "Drammenshallen",
+		pos: {
+			lat: 59.952769,
+			lng: 10.945199
+		},
+		img: "https://via.placeholder.com/200x100",
+		calendarLink: {
+			title: "My birthday party111",
+			description: "Be there!",
+			start: "2019-12-29 18:00:00 +0100",
+			end: "2019-11-29 18:00:00 +0100",
+			allDay: true
+		}
+
+	})
+
 
 	return (
 		<>
@@ -14,15 +37,13 @@ const EventsPanel = () => {
 					<Card.Title>Opprett arrangement</Card.Title>
 						<Form>
 							<hr/>
-								<EventsPanelDate/>
+								<EventsPanelDate
+									start={event.date.start}
+									end={event.date.end}
+									onChange={(date) => {setEvent({date:date})}}
+								/>
 							<hr/>
-							<Form.Group>
-								<Form.Label>Lokasjon</Form.Label>
-								<Form.Text className="text-muted">Adresse</Form.Text>
-								<Form.Control type="text" placeholder="Kort beskrivelse" />
-								<br/>
 								<EventsPanelMap/>
-							</Form.Group>
 							<hr/>
 								<EventsPanelImg/>
 							<hr/>
@@ -32,7 +53,7 @@ const EventsPanel = () => {
 				<Card.Footer>
 					<Row>
 						<Col sm={2}>
-							<Button>Lagre arrangement</Button>
+							<Button onClick={()=>console.log(event)}>Lagre arrangement</Button>
 							<Button>Forhåndvisning</Button>
 
 						</Col>
--- client/src/components/webpanel/events/EventsPanelDate.js
+++ client/src/components/webpanel/events/EventsPanelDate.js
@@ -1,12 +1,38 @@
 import {Form} from "react-bootstrap";
 import DateRangePicker from "@wojtekmaj/react-daterange-picker";
 import TimeRangePicker from "@wojtekmaj/react-timerange-picker";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+import * as dayjs from 'dayjs'
 
+const EventsPanelDate = (props) => {
 
-const EventsPanelDate = () => {
-    const [dateValue, onDateChange] = useState(new Date());
-    const [timeValue, onTimeChange] = useState(['10:00', '11:00']);
+    // string format of hour:minutes
+    const startTime = (dayjs(props.start*1000).format("hh:mm"))
+    const endTime = (dayjs(props.end*1000).format("hh:mm"))
+
+    const [dateValue, onDateChange] = useState([new Date(props.start*1000), new Date(props.end*1000)]);
+    const [timeValue, onTimeChange] = useState([startTime, endTime]);
+
+    // whenever the date changes
+    useEffect(() => {
+        props.onChange({start: dateValue[0], end: dateValue[1]})
+        },[dateValue])
+
+    // whenever the time changes
+    useEffect(() => {
+        // get the hour and the minutes exactly by splitting via the colon symbol, ':'
+        const startTime = timeValue[0].split(":");
+        const endTime = timeValue[1].split(":");
+
+        // update the dates hours and minutes
+        const startDate = dayjs(dateValue[0]).hour(startTime[0]).minute(startTime[1])
+        const endDate = dayjs(dateValue[1]).hour(endTime[0]).minute(endTime[1])
+
+        // update our dates state
+        onDateChange([startDate, endDate])
+    },[timeValue])
+
+    console.log(props)
 
     return(
         <>
@@ -21,7 +47,6 @@ const EventsPanelDate = () => {
                         value={dateValue}
                         required={true}
                     />
-                    {console.log(dateValue)}
                     <div>
                         <Form.Text className="text-muted">
                             Klokkeslett
@@ -32,7 +57,6 @@ const EventsPanelDate = () => {
                             disableClock={true}
                             required={true}
                         />
-                        {console.log(timeValue)}
                     </div>
                 </div>
             </Form.Group>
