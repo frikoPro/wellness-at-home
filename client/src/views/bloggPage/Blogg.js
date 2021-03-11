9c882d0cd295970ed90662a92ac0923cc99764f7
Ole Norholm <olenorholm55@gmail.com>
2021-03-11 15:42:42 +0100

    updated

--- client/src/views/bloggPage/Blogg.js
+++ client/src/views/bloggPage/Blogg.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Card } from 'react-bootstrap';
-
+import styles from './Blogg.module.css';
 const axios = require('axios');
 
 const Blogg = () => {
