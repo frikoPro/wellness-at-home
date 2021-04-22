1445e8c3bf2cc1c46bb633745d4b323445c1bcef
Ole Norholm <olenorholm55@gmail.com>
2021-04-22 14:26:39 +0200

    new

--- client/src/components/webpanel/events/EventsPanel.js
+++ client/src/components/webpanel/events/EventsPanel.js
@@ -1,49 +1,54 @@
-import {Button, Card, Col, Form, Row} from "react-bootstrap";
-import React, {useState} from "react";
-import EventsPanelDate from "./EventsPanelDate";
-import EventsPanelAddress from "./EventsPanelAddress";
-import EventsPanelImg from "./EventsPanelImg";
-import EventsPanelInfo from "./EventsPanelInfo";
-import UseForm from "../UseForm";
-import EventPage from "../../../views/eventsPage/EventPage";
-import Gmap from "./Gmap";
+import {Button, Card, Col, Form, Row} from 'react-bootstrap';
+import React, {useContext, useEffect, useState} from 'react';
+import EventsPanelDate from './EventsPanelDate';
+import EventsPanelAddress from './EventsPanelAddress';
+import EventsPanelImg from './EventsPanelImg';
+import EventsPanelInfo from './EventsPanelInfo';
+import EventPage from '../../../views/eventsPage/EventPage';
+import {EventContext} from '../../../contexts/EventContext';
+import Gmap from './Gmap';
 
 const EventsPanel = () => {
-	const [preview, setPreview] = useState(false)
-	const [previewBtnState, setPreviewButtonState] = useState(true)
+	const [preview, setPreview] = useState(false);
 	const triggerPreview = () => {
-		setPreview(!preview)
-	}
+		setPreview(!preview);
+	};
 	const [event2, setEvent] = useState({
-			create_date: 1676639434,
-			update_date: 1676639434,
-			date: {
-				date_start: 0,
-				date_end: 0
-			},
-			address: {
-				streetname: "",
-				city: "",
-				postalnr: ""
-			},
-			venue: "",
-			pos: {
-				lat: 59.73521,
-				lng: 10.20528
-			},
-			meta: {
-				weekdays: [ // Think ill remove weekdays, and if needed just add them as text
-					{day: "Fredag", start: 1619164800, end: 1619175600},
-					{day: "Lørdag", start: 1619254800, end: 1619262000},
-					{day: "Søndag", start: 1619341200, end: 1619352000},
-				],
-				desc: ""
-			},
-			img: "https://dhk.no/wp-content/uploads/sites/7/2019/07/Drammenshallen.jpg",
+		create_date: 1676639434,
+		update_date: 1676639434,
+		date: {
+			date_start: null,
+			date_end: null,
 		},
-	)
-	const {values, handleChange, handleEvent, handleImages, submitData} = UseForm(
-		{initialValues: {event2}, url: "http://localhost:8080/events"})
+		address: {
+			streetname: '',
+			city: '',
+			postalnr: '',
+		},
+		venue: '',
+		pos: {
+			lat: 59.73521,
+			lng: 10.20528,
+		},
+		meta: {
+			weekdays: [
+				// Think ill remove weekdays, and if needed just add them as text
+				{day: 'Fredag', start: 1619164800, end: 1619175600},
+				{day: 'Lørdag', start: 1619254800, end: 1619262000},
+				{day: 'Søndag', start: 1619341200, end: 1619352000},
+			],
+			desc: '',
+		},
+		img: '',
+	});
+
+	const {postData, returnErrors, onSuccess, cleanUp} = useContext(
+		EventContext
+	);
+
+	useEffect(() => {
+		return () => cleanUp();
+	}, []);
 
 	return (
 		<>
@@ -54,8 +59,9 @@ const EventsPanel = () => {
 						<hr/>
 						<EventsPanelInfo
 							onVenueChange={(venue) => {
-								setEvent({...event2, venue})
+								setEvent({...event2, venue});
 							}}
+							errors={returnErrors}
 							onTextChange={(description) => {
 								setEvent({
 									// dump the contents of event2
@@ -63,55 +69,68 @@ const EventsPanel = () => {
 									meta: {
 										// dump the contents of event2.meta into meta
 										...event2.meta,
-										desc: description
-									}
-								})
+										desc: description,
+									},
+								});
 							}}
 						/>
 						<EventsPanelDate
+							errors={returnErrors}
 							onChange={(date) => {
-								setEvent({...event2, date: {date_start: date[0], date_end: date[1]}})
+								setEvent({
+									...event2,
+									date: {date_start: date[0], date_end: date[1]},
+								});
 							}}
 						/>
 						<hr/>
 						<Gmap
-							onPlacesChanged={(address) => setEvent({
-								...event2,
-								address: {
-									streetname: address[0] + " " + address[1],
-									city: address[2],
-									postalnr: address[3]
-								},
-								pos: {lat: address[4], lng: address[5]}
-							})}
+							onPlacesChanged={(address) =>
+								setEvent({
+									...event2,
+									address: {
+										streetname: address[0] + ' ' + address[1],
+										city: address[2],
+										postalnr: address[3],
+									},
+									pos: {lat: address[4], lng: address[5]},
+								})
+							}
 						/>
 						<EventsPanelAddress
+							errors={returnErrors}
 							address={event2.address}
 							onStreetChange={(streetname) => {
 								setEvent({
-									...event2, address: {
-										...event2.address, streetname: streetname
-									}
-								})
+									...event2,
+									address: {
+										...event2.address,
+										streetname: streetname,
+									},
+								});
 							}}
 							onCityChange={(city) => {
 								setEvent({
-									...event2, address: {
-										...event2.address, city: city
-									}
-								})
+									...event2,
+									address: {
+										...event2.address,
+										city: city,
+									},
+								});
 							}}
 							onPostalCodeChange={(postalcode) => {
 								setEvent({
-									...event2, address: {
-										...event2.address, postalcode: postalcode
-									}
-								})
+									...event2,
+									address: {
+										...event2.address,
+										postalcode: postalcode,
+									},
+								});
 							}}
 						/>
 						<hr/>
 						<EventsPanelImg
-							handleImage={handleImages}
+							onChange={(file) => setEvent({...event2, img: file})}
 						/>
 					</Form>
 				</Card.Body>
@@ -125,18 +144,19 @@ const EventsPanel = () => {
 							<br/>
 							<Button onClick={() => triggerPreview()}>Forhåndvisning</Button>
 						</Col>
-						<Col className="align-self-center">
-						</Col>
+						<Col className="align-self-center text-success">{onSuccess}</Col>
 					</Row>
 				</Card.Footer>
 			</Card>
-			{preview ? <Card>
-				<EventPage
-					{...event2}
-				/>
-			</Card> : <></>}
+			{preview ? (
+				<Card>
+					<EventPage {...event2} />
+				</Card>
+			) : (
+				<></>
+			)}
 		</>
-	)
+	);
 };
 
 export default EventsPanel;
