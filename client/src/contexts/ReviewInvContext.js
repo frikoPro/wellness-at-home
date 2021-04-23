369226a09b39cf3c4551147426dcd476ac4e2d85
FrikoPro <jansenfredrik@gmail.com>
2021-04-23 13:54:37 +0200

    Updates to invites now happens in context.

--- client/src/contexts/ReviewInvContext.js
+++ client/src/contexts/ReviewInvContext.js
@@ -5,15 +5,56 @@ export const ReviewInvContext = createContext();
 
 export const ReviewInvProvider = (props) => {
 	const [invites, setInvites] = useState([]);
+	const [onSuccess, setSuccess] = useState(null);
+	const [errors, setErrors] = useState(null);
+
+	const url = 'http://localhost:8080/reviewinvites/';
 
 	useEffect(() => {
+		fetchData();
+		setSuccess(null);
+		setErrors(null);
+	}, [onSuccess]);
+
+	const fetchData = () => {
+		axios.get(url).then((res) => setInvites(res.data));
+	};
+
+	const postData = (item) => {
 		axios
-			.get('http://localhost:8080/reviewinvites/')
-			.then((res) => setInvites(res.data));
-	}, []);
+			.post(url + 'add', item)
+			.then((res) => setSuccess(res.data))
+			.catch((err) => setErrors(err.response.data));
+	};
+
+	const deleteData = (index) => {
+		const { _id } = invites[index];
+
+		console.log(_id);
+
+		axios
+			.delete(url + _id)
+			.then((res) => setSuccess(res.data))
+			.catch((err) => console.log(err));
+	};
+
+	const returnError = (field) => {
+		if (errors) {
+			const index = errors.fields.findIndex((err) => err === field);
+
+			return errors.messages[index];
+		}
+	};
 
 	return (
-		<ReviewInvContext.Provider value={invites}>
+		<ReviewInvContext.Provider
+			value={{
+				invites: invites,
+				submitData: postData,
+				deleteData: deleteData,
+				returnErrors: returnError,
+				onSuccess: onSuccess,
+			}}>
 			{props.children}
 		</ReviewInvContext.Provider>
 	);
