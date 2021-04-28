761387fd8570004cb996e58c1927c8b76c051786
FrikoPro <jansenfredrik@gmail.com>
2021-04-28 19:59:53 +0200

    Added hover effect and a icon on FAQ.

--- client/src/components/FAQ/FAQ.js
+++ client/src/components/FAQ/FAQ.js
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react';
 import { LoggedInContext } from '../../contexts/LoggedInContext';
-import { Accordion, Button, Card } from 'react-bootstrap';
+import { Accordion, Button, Card, Row, Col } from 'react-bootstrap';
 import UseForm from '../webpanel/UseForm';
 import AddFAQModal from './AddFAQModal';
 
@@ -24,8 +24,23 @@ const FAQ = ({
 	return (
 		<Accordion>
 			<Card style={{ cursor: 'pointer' }}>
-				<Accordion.Toggle as={Card.Header} eventKey="0">
-					{question}
+				<Accordion.Toggle as={Card.Header} eventKey="0" className="hover-gold">
+					<Row>
+						<Col xs={9}>
+							<span>{question}</span>
+						</Col>
+						<Col className="text-right">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="24px"
+								viewBox="0 0 24 24"
+								width="24px"
+								fill="#000000">
+								<path d="M0 0h24v24H0V0z" fill="none" />
+								<path d="M7 10l5 5 5-5H7z" />
+							</svg>
+						</Col>
+					</Row>
 				</Accordion.Toggle>
 				<Accordion.Collapse eventKey="0">
 					<Card.Body>{answer}</Card.Body>
