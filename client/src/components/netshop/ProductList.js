11f23918fb557675833b9fcc7c987d1eee39d9cf
FrikoPro <jansenfredrik@gmail.com>
2021-04-28 12:33:57 +0200

    changed margin top to bottom

--- client/src/components/netshop/ProductList.js
+++ client/src/components/netshop/ProductList.js
@@ -2,11 +2,11 @@ import { Col } from 'react-bootstrap';
 import ProductCard from './ProductCard';
 
 const ProductList = ({ products }) => {
-  return products.map((item, i) => (
-    <Col sm={3} className="mt-5" key={i}>
-      <ProductCard image={item.images[0].image} {...item} />
-    </Col>
-  ));
+	return products.map((item, i) => (
+		<Col sm={3} className="mb-5" key={i}>
+			<ProductCard image={item.images[0].image} {...item} />
+		</Col>
+	));
 };
 
 export default ProductList;
