6ee35cc33e6f4b0a5abe09e281b1639a4b8615a0
Cenxus <urs_ole@hotmail.com>
2021-04-30 11:43:12 +0200

    Update SupplierPage.js
    Added <Col> between Rows

--- client/src/views/SupplierPage/SupplierPage.js
+++ client/src/views/SupplierPage/SupplierPage.js
@@ -39,64 +39,83 @@ const SupplierPage = () => {
 			<Row
 				className="justify-content-center p-5"
 				style={{ backgroundColor: '#F2F3F7' }}>
-				<img src={getLogo()} alt="" style={{ width: '35%' }}></img>
+				<Col sm={6}>
+					<img src={getLogo()} alt="" className="w-100"></img>
+				</Col>
+				
 			</Row>
 			<Row className="justify-content-center m-4">
-				<h5>
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-					accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-					ab illo inventore veritatis et quasi architecto beatae vitae dicta
-					sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-					aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-					qui ratione voluptatem sequi nesciunt.
-				</h5>
+				<Col>
+					<h5>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+						ab illo inventore veritatis et quasi architecto beatae vitae dicta
+						sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+						qui ratione voluptatem sequi nesciunt.
+					</h5>
+				</Col>
 			</Row>
-			<Row className="justify-content-center mb-1">
-				<h2>{id}s spabad</h2>
+			<Row className="justify-content-center mb-1 text-center">
+				<Col>
+					<h2>{id}s spabad</h2>
+				</Col>
 			</Row>
 			<section>
 				<Row className="shadow border border-dark m-2">
 					<Col className="m-3">
 						<Row className="justify-content-center">
-							<img
-								style={{ width: '50%' }}
-								src={
-									mappedJacuzzis[currentJacuzzi] &&
-									`/${mappedJacuzzis[currentJacuzzi].image}`
-								}
-								alt=""></img>
-						</Row>
-						<Row className="justify-content-center">
-							<h1>
-								{mappedJacuzzis[currentJacuzzi] &&
-									mappedJacuzzis[currentJacuzzi].textHead}
-							</h1>
+							<Col sm={7}>
+								<img
+									className="w-100"
+									src={
+										mappedJacuzzis[currentJacuzzi] &&
+										`/${mappedJacuzzis[currentJacuzzi].image}`
+									}
+									alt="">
+								</img>
+							</Col>
 						</Row>
 						<Row className="justify-content-center text-center">
-							<p>
-								{mappedJacuzzis[currentJacuzzi] &&
-									mappedJacuzzis[currentJacuzzi].about}
-							</p>
+							<Col>
+								<h1>
+									{mappedJacuzzis[currentJacuzzi] &&
+										mappedJacuzzis[currentJacuzzi].textHead}
+								</h1>
+							</Col>
 						</Row>
-						<Row className="justify-content-center">
-							<Button
-								as={Link}
-								to={
-									mappedJacuzzis[currentJacuzzi] &&
-									`/spabad/${mappedJacuzzis[currentJacuzzi].link}`
-								}
-								className="shadow btn-warning">
-								Les mer
-							</Button>
+						<Row className="justify-content-center text-center">
+							<Col sm={10}>
+								<p>
+									{mappedJacuzzis[currentJacuzzi] &&
+										mappedJacuzzis[currentJacuzzi].about}
+								</p>
+							</Col>
+						</Row>
+						<Row>
+							<Col sm={12} className="justify-content-center text-center">
+								<Button
+									as={Link}
+									to={
+										mappedJacuzzis[currentJacuzzi] &&
+										`/spabad/${mappedJacuzzis[currentJacuzzi].link}`
+									}
+									className="shadow btn-warning">
+									Les mer
+								</Button>
+							</Col>
 						</Row>
 					</Col>
 				</Row>
 				<Row>
-					<ScrollDiv
-						content={mappedJacuzzis}
-						returnFunction={(index) => setCurrentJacuzzi(index)}
-						size={3}
-					/>
+					<Col>
+						<ScrollDiv
+							content={mappedJacuzzis}
+							returnFunction={(index) => setCurrentJacuzzi(index)}
+							size={3}
+						/>
+					</Col>
+					
 				</Row>
 			</section>
 		</Container>
