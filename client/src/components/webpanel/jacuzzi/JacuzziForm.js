93ea0d4b37e9f34e82f4e0aaad765917851cbec9
FrikoPro <jansenfredrik@gmail.com>
2021-05-04 15:42:49 +0200

    Removed related products from accessories in backend and frontend webpanel.

--- client/src/components/webpanel/jacuzzi/JacuzziForm.js
+++ client/src/components/webpanel/jacuzzi/JacuzziForm.js
@@ -3,6 +3,9 @@ import { JacuzziContext } from '../../../contexts/JacuzziContext';
 import { useContext } from 'react';
 import SelectAddText from '../SelectAddText';
 import UserForm from '../UserForm';
+import { ProductsContext } from '../../../contexts/ProductsContext';
+import ScrollDiv from '../../scrolldiv/ScrollDiv';
+import SelectInput from '../SelectInput';
 
 const JacuzziForm = ({
 	values,
@@ -13,6 +16,7 @@ const JacuzziForm = ({
 	removeValues,
 }) => {
 	const { brands, techSpec } = useContext(JacuzziContext);
+	const { products } = useContext(ProductsContext);
 
 	return (
 		<Form>
@@ -35,6 +39,33 @@ const JacuzziForm = ({
 				values={values}
 				techSpec={techSpec}
 			/>
+			<Form.Group>
+				<Form.Label>Relaterte produkter</Form.Label>
+				<SelectInput
+					name="relatedProducts"
+					options={products.map((item) => ({
+						value: item._id,
+						userText: item.name,
+					}))}
+					handleChange={(e) =>
+						handleEvent(e.target.name, [
+							...values.relatedProducts,
+							e.target.value,
+						])
+					}
+				/>
+				<ScrollDiv
+					content={products
+						.filter((item) => values.relatedProducts.includes(item._id))
+						.map((item) => ({
+							name: item.name,
+							image: item.images[0].image,
+							id: item._id,
+						}))}
+					returnFunction={(i, id) => removeValues('relatedProducts', id)}
+					size={3}
+				/>
+			</Form.Group>
 		</Form>
 	);
 };
