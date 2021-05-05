0dac513e12eae833fa09dac22e6487be0abee4ad
FrikoPro <jansenfredrik@gmail.com>
2021-05-05 15:56:48 +0200

    removed static content from FAQ.

--- client/src/views/supportPage/SupportPage.js
+++ client/src/views/supportPage/SupportPage.js
@@ -3,35 +3,12 @@ import FAQList from '../../components/FAQ/FAQList';
 import ContactCard from '../../components/ContactCard';
 import SupportForm from '../../components/SupportForm';
 import styles from './SupportPage.module.css';
-import {Helmet} from "react-helmet";
-import React from "react";
+import { Helmet } from 'react-helmet';
+import React from 'react';
 
 const SupportPage = () => {
-	const supportPageContent = {
-		FAQs: [
-			{
-				question: 'Hvor lang tid tar bestillingen?',
-				answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-			},
-			{
-				question: 'Har mistet brukermanual',
-				answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-			},
-			{
-				question: 'Kan man bestille med faktura?',
-				answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-			},
-			{
-				question: 'Hva er forskjellen på saltvann og klor?',
-				answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-			},
-		],
-	};
-
-	
-
 	return (
-		<Container 
+		<Container
 			className="shadow"
 			style={{ backgroundColor: 'white', marginTop: '5%' }}>
 			<Helmet>
@@ -42,8 +19,10 @@ const SupportPage = () => {
 					<h1>Ofte stilte spørsmål</h1>
 				</Col>
 			</Row>
-			<Row className="justify-content-center mx-auto ml-5" style={{width: "70%"}}>
-				<FAQList FAQs={supportPageContent.FAQs}/>
+			<Row
+				className="justify-content-center mx-auto ml-5"
+				style={{ width: '70%' }}>
+				<FAQList />
 			</Row>
 			<Row className="mt-5">
 				<Col className={'text-center'}>
@@ -53,12 +32,15 @@ const SupportPage = () => {
 			<Row>
 				<Col className={'text-center'}>
 					<p>Fyll ut skjemaet under så hjelper vi deg</p>
-					<span>Felter merket med <span style={{color:"#FF0000"}}>*</span> må fylles ut.</span>
+					<span>
+						Felter merket med <span style={{ color: '#FF0000' }}>*</span> må
+						fylles ut.
+					</span>
 				</Col>
 			</Row>
 			<Row className="justify-content-center">
 				<div className={`border border-dark p-3 ${styles.divResponsive}`}>
-					<SupportForm/>
+					<SupportForm />
 				</div>
 			</Row>
 			<Row>
@@ -66,13 +48,21 @@ const SupportPage = () => {
 					<p>Eller kontakt oss direkte</p>
 				</Col>
 			</Row>
-			
+
 			<Row className="pb-5">
 				<Col>
-					<ContactCard name="Hans Petter Sandvold" phone="456 20 830" email="hanspetter@wellnessathome.no"/>
+					<ContactCard
+						name="Hans Petter Sandvold"
+						phone="456 20 830"
+						email="hanspetter@wellnessathome.no"
+					/>
 				</Col>
 				<Col>
-					<ContactCard name="Kenneth Nygård" phone="654 45 560" email="kenneth@wellnessathome.no"/>
+					<ContactCard
+						name="Kenneth Nygård"
+						phone="654 45 560"
+						email="kenneth@wellnessathome.no"
+					/>
 				</Col>
 			</Row>
 		</Container>
