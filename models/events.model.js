a09f002d0f8393481c67d1f357a80fc30ea7fa9e
FrikoPro <jansenfredrik@gmail.com>
2021-05-06 14:42:23 +0200

    Changed data structure in events backend.

--- models/events.model.js
+++ models/events.model.js
@@ -21,10 +21,7 @@ const EventSchema = new Schema(
 		venue: { type: String, required: [true, 'Må ha et stedsnavn'] },
 		pos: { lat: Number, lng: Number },
 		meta: {
-			weekdays: {
-				type: [{ day: String, start: Number, end: Number }],
-				_id: false,
-			},
+			weekdays: String,
 			desc: String,
 		},
 		img: String,
